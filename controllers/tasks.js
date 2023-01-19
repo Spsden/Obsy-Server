14b65726e850008fb975eee24465e66459431898
Spsden <pratapsinghsuraj420@gmail.com>
2023-01-19 17:57:46 +0530

    db interfaces done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,35 @@
-const getAllTasks = (req, res) => {
-  res.send("All items");
+const Task = require("../models/task");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({id:req.params.id});
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with Id` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+  
 };
 
 const updateTask = (req, res) => {
@@ -23,5 +45,5 @@ module.exports = {
   createTask,
   getTask,
   updateTask,
-  deleteTask
+  deleteTask,
 };
--- models/task.js
+++ models/task.js
@@ -1,15 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ObsyTaskSchema = new mongoose.Schema({ 
-    title:String,
-    durationType:String,
-    icon:String,
-    localiD:String,
-    interval:Number,
-    duration:String,
-    url:String,
-    xPath:String,
-    date:Date,
-})
+const ObsyTaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "title required"],
+  },
+  durationType: {
+    type: String,
+    required: [true, "duration type required"],
+  },
+  icon: {
+    type: String,
+    required: [true, "icon required"],
+  },
+  localId: {
+    type: String,
+    required: [true, "localId required"],
+  },
+  interval: {
+    type: Number,
+    required: [true, "interval required"],
+  },
+  duration: {
+    type: String,
+    required: [true, "duration required"],
+  },
+  url: {
+    type: String,
+    required: [true, "url required"],
+  },
+  xPath: {
+    type: String,
+    required: [true, "xPath required"],
+  },
+  date: Date,
+});
 
-module.exports = mongoose.model('Task',ObsyTaskSchema)
+module.exports = mongoose.model("Task", ObsyTaskSchema);
