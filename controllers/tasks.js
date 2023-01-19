9e8664a0a37568a9e1378292415f00703928c778
Spsden <pratapsinghsuraj420@gmail.com>
2023-01-19 18:06:40 +0530

    delete task implemented

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -29,15 +29,25 @@ const getTask = async (req, res) => {
   } catch (error) {
     res.status(500).json({ msg: error });
   }
-  
 };
 
 const updateTask = (req, res) => {
   res.send("update task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskId}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
