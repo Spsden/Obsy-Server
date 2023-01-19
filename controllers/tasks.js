6d6c57105d2ae8664199aff2e6bbea968d117839
Spsden <pratapsinghsuraj420@gmail.com>
2023-01-19 15:28:05 +0530

    skeleton set

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,26 @@
-const getAllTasks = (req,res) => {
-    res.send("All items")
-}
+const getAllTasks = (req, res) => {
+  res.send("All items");
+};
+
+const createTask = (req, res) => {
+  res.send("create task");
+};
+
+const getTask = (req, res) => {
+  res.send("get a single task");
+};
+
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
 
 module.exports = {
-    getAllTasks
-}
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
+};
